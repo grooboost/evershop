29ea70d68c6896e152e659bfe3fb65394e2b5243
ubermen <kjh3477@gmail.com>
2025-03-30 08:29:59 +0900

    Fix svg icon expression issue due to dash

--- extensions/firebase_login/pages/frontStore/login/icons/NaverIcon.jsx
+++ extensions/firebase_login/pages/frontStore/login/icons/NaverIcon.jsx
@@ -5,7 +5,7 @@ function NaverIcon({ width, height }) {
   return (
     <svg width={width} height={height} viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="0.5" width={width} height={height} rx="9" fill="#BDBDBD"/>
-    <g clip-path="url(#clip0_881_14293)">
+    <g clipPath="url(#clip0_881_14293)">
     <path d="M10.7465 9.24617L8.15113 5.5H6V12.5H8.25351V8.75325L10.8489 12.5H13V5.5H10.7465V9.24617Z" fill="white"/>
     </g>
     <defs>
