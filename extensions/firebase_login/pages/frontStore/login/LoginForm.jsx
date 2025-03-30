e901165aa89c1da2dfc956c3b495e44c93a9b65d
ubermen <kjh3477@gmail.com>
2025-03-30 11:35:19 +0900

    Modify firebase login form layout

--- extensions/firebase_login/pages/frontStore/login/LoginForm.jsx
+++ extensions/firebase_login/pages/frontStore/login/LoginForm.jsx
@@ -8,11 +8,11 @@ function getMailToLink(os, browser) {
   const subject = encodeURIComponent("로그인 문제 도움 요청");
   const body = encodeURIComponent(
     `안녕하세요, 로그인 중 문제가 발생했습니다.\n\n` +
+    `아래에 문제에 대한 상세 내용을 기술해 주세요:\n\n\n\n` +
     `---\n` +
     `OS 정보: ${os}\n` +
     `브라우저 환경 정보: ${browser}\n` +
-    `---\n` +
-    `아래에 문제에 대한 상세 내용을 기술해 주세요:\n`
+    `---\n`
   );
 
   return `mailto:otable.help@gmail.com?subject=${subject}&body=${body}`;
@@ -37,10 +37,10 @@ export default function LoginForm({
   }, []);
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center" >
       <div className="login-form flex justify-center items-center">
         <div className="login-form-inner">
-          <div>
+          <div style={{ height: '33.34vh', maxHeight: '300px'}}>
             <h1 className="text-left">{`쉬운 집밥의 시작,\n오디너리테이블`}</h1>
             <h2 className="text-left">{`환영합니다! 함께 만족스러운 식탁 만들어요.`}</h2>
             {error && <div className="text-critical mb-4">{error}</div>}
